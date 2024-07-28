2e88190da72d57532f11fbfed4404bf35b36d617
therealmazin <mazinksa@live.com>
2024-07-27 21:48:43 -0500

    Update rap.py

--- rap.py
+++ rap.py
@@ -20,35 +20,3 @@ def get_audio_information(audio_ids):
     except ValueError:
         print(f"Non-JSON response received for audio information: {response.text}")
         return None
-
-# def convert_to_wav(audio_id):
-#     url = f"{base_url}/api/gen/{audio_id}/convert_wav/"
-#     response = requests.post(url)
-#     try:
-#         return response.json()
-#     except ValueError:
-#         print(f"Non-JSON response received for convert to wav: {response.text}")
-#         return None
-
-# def get_wav_file_url(audio_id):
-#     url = f"{base_url}/api/gen/{audio_id}/wav_file/"
-#     response = requests.get(url)
-#     try:
-#         return response.json()
-#     except ValueError:
-#         print(f"Non-JSON response received for wav file url: {response.text}")
-#         return None
-
-# def download_audio(url, file_name):
-#     response = requests.get(url)
-#     with open(file_name, 'wb') as f:
-#         f.write(response.content)
-
-# def process_audio_generation(payload):
-#     data = generate_audio_by_prompt(payload)
-    
-#     if not data:
-#         return "Failed to generate audio from prompt", None
-    
-#     ids = f"{data[0]['id']},{data[1]['id']}"
-#     print(f"ids: {ids}")
